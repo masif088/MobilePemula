e3584a339ed79317beb2b36dc1eea2257dc79f54
masif088 <mokhamadasif@gmail.com>
2019-09-09 19:56:30 +0700

    dicoding pemula

---
+++ app/src/main/java/com/example/mobilepemula/Adapter.java
@@ -0,0 +1,12 @@
+package com.example.mobilepemula;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+}
+    }
+}
